48e10d6507e0ab94032439d6cca822215b858309
Alex <alexjair584@gmail.com>
2025-02-14 23:50:20 -0500

    Agregando atributos a los DTO

--- gestion-productos/src/main/java/com/proyecto/gestion/dto/CategoriaDTO.java
+++ gestion-productos/src/main/java/com/proyecto/gestion/dto/CategoriaDTO.java
@@ -2,4 +2,9 @@ package com.proyecto.gestion.dto;
 
 public class CategoriaDTO {
 
+	private Integer id;
+	
+	private String nombre_cat;
+	
+	
 }
--- gestion-productos/src/main/java/com/proyecto/gestion/dto/ProductoDTO.java
+++ gestion-productos/src/main/java/com/proyecto/gestion/dto/ProductoDTO.java
@@ -1,5 +1,18 @@
 package com.proyecto.gestion.dto;
 
+
 public class ProductoDTO {
 
+	
+	private Integer id;
+	
+	private String nombre_prod;
+	
+	private Double precio_unitario;
+	
+	private Integer cantidad;
+	
+	private Integer idCategoria;
+	
+	
 }
